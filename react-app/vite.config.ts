1b10e322b3909b870a345f3b676f169f8153398d
Jezapj <jezapj@gmail.com>
2026-06-13 12:13:27 +1000

    Updated vite settings for vercel

--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Ensure production assets are mapped to the server root URL
+  publicDir: 'public', // Explicitly force Vite to bundle your public static folder
   server: {
     host: '0.0.0.0',
     port: 3000,
